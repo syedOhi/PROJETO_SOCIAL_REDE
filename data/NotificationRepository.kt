4e9dd1e0c2444b0adae359b6a209adf129aacd33
syedOhi <119738392+syedOhi@users.noreply.github.com>
2026-01-29 19:28:56 +0000

    Delete data directory

--- data/NotificationRepository.kt
+++
@@ -1,9 +0,0 @@
-// --- 3. Notification Repository ---
-package com.example.social_rede_mobile.data
-
-class NotificationRepository(private val dao: NotificationDao) {
-    fun getAllNotifications() = dao.getAllNotifications()
-    suspend fun insert(notification: Notification) = dao.insertNotification(notification)
-    suspend fun markSeen(id: String) = dao.markAsSeen(id)
-    suspend fun delete(notification: Notification) = dao.deleteNotification(notification)
-}
